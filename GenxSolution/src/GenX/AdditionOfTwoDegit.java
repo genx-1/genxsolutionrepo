7b6224c6dc70089566e4eba824e29e4362a24a46
genx-1 <156506319+genx-1@users.noreply.github.com>
2024-11-19 19:22:41 +0530

    Update AdditionOfTwoDegit.java
    deleted code and added *

--- GenxSolution/src/GenX/AdditionOfTwoDegit.java
+++ GenxSolution/src/GenX/AdditionOfTwoDegit.java
@@ -3,10 +3,9 @@ package GenX;
 public class AdditionOfTwoDegit {
 
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 800;
-		int c = a+b;
-		System.out.println("Addition of two number "+c);
+	**********************************************************
+****************************************************************
+		**********************************************
 
 	}
 
